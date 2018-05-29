171f5a9ae6c15a9175b566e95e8b811cd25dc906
Barke Said <barke993@gmail.com>
2018-05-29 14:21:29 +0300

    moved addition function to the js file

--- alerts.js
+++ alerts.js
@@ -8,3 +8,18 @@ function sayThankyou(){
     alert('Thank you for submitting');
 }
 
+
+
+     function addition(){
+    
+               var number1 =  document.getElementById("number1").value; 
+               var number2 =  document.getElementById("number2").value; 
+               var sum =  document.getElementById("sum").value; 
+
+                   sum = number1 + number2;
+
+                 document.getElementById("sum").focus();
+                 document.getElementById("sum").value=sum;
+    
+            }
+
